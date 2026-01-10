056c0621268deb9d5074b8fa41d463a948c13022
doarvid <42486682+doarvid@users.noreply.github.com>
2026-01-10 08:49:47 +0800

    feat: klinechart图表

--- plot/server.go
+++ plot/server.go
@@ -39,3 +39,43 @@ func (s *StandardHTTPServer) RegisterFileServer(path string, fs http.FileSystem)
 func (s *StandardHTTPServer) Start(port int) error {
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
+
+// MuxHTTPServer implements the HTTPServer interface using the standard http package
+type MuxHTTPServer struct {
+	mux    *http.ServeMux
+	server *http.Server
+}
+
+// NewMuxHTTPServer creates a new instance of MuxHTTPServer
+func NewMuxHTTPServer() *MuxHTTPServer {
+	mux := http.NewServeMux()
+	return &MuxHTTPServer{
+		mux: mux,
+	}
+}
+
+// RegisterHandler registers a handler for a specific route
+func (s *MuxHTTPServer) RegisterHandler(path string, handler http.HandlerFunc) {
+	s.mux.HandleFunc(path, handler)
+}
+
+// RegisterFileServer registers a handler to serve static files
+func (s *MuxHTTPServer) RegisterFileServer(path string, fs http.FileSystem) {
+	s.mux.Handle(path, http.FileServer(fs))
+}
+
+// Start starts the HTTP server on the specified port
+func (s *MuxHTTPServer) Start(port int) error {
+	s.server = &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: s.mux,
+	}
+	return s.server.ListenAndServe()
+}
+
+func (s *MuxHTTPServer) Close() error {
+	if s.server == nil {
+		return nil
+	}
+	return s.server.Close()
+}
